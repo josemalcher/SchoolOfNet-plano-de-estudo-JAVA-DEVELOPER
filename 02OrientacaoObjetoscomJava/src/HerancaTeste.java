97d16ae7a0d35fe247bfce48b11be02edc67deda
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-11-02 16:45:40 -0300

    12 - Herança + Ajustes

--- 02OrientacaoObjetoscomJava/src/HerancaTeste.java
+++ 02OrientacaoObjetoscomJava/src/HerancaTeste.java
@@ -2,7 +2,7 @@ public class HerancaTeste {
     public static void main(String[] args) {
         HerancaVeiculoCarro carro01 = new HerancaVeiculoCarro();
 
-        carro01.
+        carro01.setCor("Preta");
         carro01.setMachas(6);
         carro01.setVelocidadeMaxima(300.0);
 
